480822cd155cfbed18f1013c30b64558685fdad9
DevanshiKaneria <devanshikaneria29@gmail.com>
2025-09-06 23:04:03 +0530

    labwork4

---
+++ BillionaireAnalysis[1].py
@@ -0,0 +1,60 @@
+import pandas as pa
+import numpy as nu
+import seaborn as se
+import matplotlib.pyplot as plt
+
+data = pa.read_csv("D:\python project\Billionaire.csv")
+print(data.head(10))
+
+df = data.sort_values(by = ["NetWorth"],ascending=False).head(10)
+plt.figure(figsize=(20,8))
+se.histplot(x="Name",hue="NetWorth",data=df)
+plt.savefig('D:\python project\Histogram.png')
+plt.show()
+
+a = data["Source"].value_counts().head()
+index = a.index
+sources = a.values
+custom_colors = ["skyblue", "yellowgreen", 'tomato', "blue", "red"]
+plt.figure(figsize=(7, 7))
+plt.pie(sources, labels=index, colors=custom_colors)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Top 5 Domains to Become a Billionaire", fontsize=20)
+plt.savefig('D:\python project\Domains.png')
+plt.show()
+
+a = data["Industry"].value_counts().head()
+index = a.index
+industries = a.values
+custom_colors = ["skyblue", "yellowgreen", 'tomato', "blue", "red"]
+plt.figure(figsize=(10, 10))
+plt.pie(industries, labels=index, colors=custom_colors)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Top 5 Industries with Most Number of Billionaires", fontsize=20)
+plt.savefig('D:\python project\Industries.png')
+plt.show()
+
+
+a = data["Country"].value_counts().head()
+index = a.index
+Countries = a.values
+custom_colors = ["skyblue", "yellowgreen", 'tomato', "blue", "red"]
+plt.figure(figsize=(7, 7))
+plt.pie(Countries, labels=index, colors=custom_colors)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Top 5 Countries with Most Number of Billionaires", fontsize=20)
+plt.savefig('D:\python project\Countries.png')
+plt.show()
+
+
+
+
